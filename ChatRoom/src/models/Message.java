5771b16c82bebae1d9b9da96434bd6a24627add1
K-Kabiri <126855522+K-Kabiri@users.noreply.github.com>
2023-06-20 03:53:59 +0330

    Added insertMessage functions for PV-Databases.

--- ChatRoom/src/models/Message.java
+++ ChatRoom/src/models/Message.java
@@ -10,6 +10,12 @@ public class Message {
     private int userID;
     private String message;
     private static int messageID = 1;
+    private static int messageIDClient12 = 1;
+    private static int messageIDClient13 = 1;
+    private static int messageIDClient14 = 1;
+    private static int messageIDClient23 = 1;
+    private static int messageIDClient24 = 1;
+    private static int messageIDClient34 = 1;
 
     public Message(String username , int userID, String message) {
         this.userID = userID;
@@ -22,9 +28,39 @@ public class Message {
         String sql = String.format("INSERT INTO `messages` (`messageID`, `userID`, `username`, `message`) VALUES (%s, %s,'%s' ,'%s')" , messageID++ , userID , username , message );
         mySQLConnection.executeSQL(sql);
     }
-    public ResultSet selectMessage(int userID ,String username) throws SQLException, ClassNotFoundException {
+    public static ResultSet selectMessage() throws SQLException, ClassNotFoundException {
         MySQLConnection mySQLConnection = new MySQLConnection();
-        String query = String.format("SELECT username , message FROM `messages`");
+        String query = String.format("SELECT username , message FROM `messages` ORDER BY messageID ASC");
         return mySQLConnection.executeQuery(query);
+    }
+    public void insertMessageIntoClient1_Client2() throws SQLException, ClassNotFoundException {
+        MySQLConnection mySQLConnection = new MySQLConnection();
+        String sql = String.format("INSERT INTO `client1-client2` (`messageID`, `userID`, `username`, `message`) VALUES (%s, %s,'%s' ,'%s')" , messageIDClient12++ , userID , username , message );
+        mySQLConnection.executeSQL(sql);
+    }
+    public void insertMessageIntoClient1_Client3() throws SQLException, ClassNotFoundException {
+        MySQLConnection mySQLConnection = new MySQLConnection();
+        String sql = String.format("INSERT INTO `client1-client3` (`messageID`, `userID`, `username`, `message`) VALUES (%s, %s,'%s' ,'%s')" , messageIDClient13++ , userID , username , message );
+        mySQLConnection.executeSQL(sql);
+    }
+    public void insertMessageIntoClient1_Client4() throws SQLException, ClassNotFoundException {
+        MySQLConnection mySQLConnection = new MySQLConnection();
+        String sql = String.format("INSERT INTO `client1-client4` (`messageID`, `userID`, `username`, `message`) VALUES (%s, %s,'%s' ,'%s')" , messageIDClient14++ , userID , username , message );
+        mySQLConnection.executeSQL(sql);
+    }
+    public void insertMessageIntoClient2_Client3() throws SQLException, ClassNotFoundException {
+        MySQLConnection mySQLConnection = new MySQLConnection();
+        String sql = String.format("INSERT INTO `client2-client3` (`messageID`, `userID`, `username`, `message`) VALUES (%s, %s,'%s' ,'%s')" , messageIDClient23++ , userID , username , message );
+        mySQLConnection.executeSQL(sql);
+    }
+    public void insertMessageIntoClient2_Client4() throws SQLException, ClassNotFoundException {
+        MySQLConnection mySQLConnection = new MySQLConnection();
+        String sql = String.format("INSERT INTO `client2-client4` (`messageID`, `userID`, `username`, `message`) VALUES (%s, %s,'%s' ,'%s')" , messageIDClient24++ , userID , username , message );
+        mySQLConnection.executeSQL(sql);
+    }
+    public void insertMessageIntoClient3_Client4() throws SQLException, ClassNotFoundException {
+        MySQLConnection mySQLConnection = new MySQLConnection();
+        String sql = String.format("INSERT INTO `client3-client4` (`messageID`, `userID`, `username`, `message`) VALUES (%s, %s,'%s' ,'%s')" , messageIDClient34++ , userID , username , message );
+        mySQLConnection.executeSQL(sql);
     }
 }
